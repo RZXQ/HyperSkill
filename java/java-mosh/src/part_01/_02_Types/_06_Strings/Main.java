102ca567247f02fae3f4a99721e7c3ea2c3596e3
Reacher <31989133+RZXQ@users.noreply.github.com>
2024-09-26 00:49:31 +0800

    add: _06_Strings java file

---
+++ java/java-mosh/src/part_01/_02_Types/_06_Strings/Main.java
@@ -0,0 +1,28 @@
+package part_01._02_Types._06_Strings;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Initialize string
+		String message = "  Hello World" + "!!  ";
+		System.out.println(message);
+
+		// Check start and end
+		System.out.println(message.startsWith("!!"));
+		System.out.println(message.endsWith("!!"));
+
+		// Length and index
+		System.out.println(message.length());
+		System.out.println(message.indexOf("sky"));
+
+		// String replacement
+		System.out.println(message.replace("!", "*"));
+
+		// String immutability and transformations
+		System.out.println(message); // Original string
+		System.out.println(message.toLowerCase());
+		System.out.println(message.toUpperCase());
+		System.out.println(message.trim());
+	}
+
+}
