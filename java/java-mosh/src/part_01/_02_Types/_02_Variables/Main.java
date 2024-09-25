bac65e9e7b3c9b9871e06573b3684f41e8ae97be
Reacher <31989133+RZXQ@users.noreply.github.com>
2024-09-25 23:26:56 +0800

    add: _02_Variables java file

---
+++ java/java-mosh/src/part_01/_02_Types/_02_Variables/Main.java
@@ -0,0 +1,14 @@
+package part_01._02_Types._02_Variables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int age = 30;
+		age = 35;
+
+		int myAge = 30; // naming variable: camel case notation
+		int herAge = myAge;
+		System.out.println(herAge);
+	}
+
+}
