834a839b81e4674d5a10896414d1074a4ccb4bc2
Reacher <31989133+RZXQ@users.noreply.github.com>
2024-09-26 01:57:29 +0800

    Add Main.java to demonstrate basic arithmetic operations
    This new file includes examples of addition, division, and increment operations in Java. It prints the results of these operations to the console for verification and learning purposes.

---
+++ java/java-mosh/src/part_01/_02_Types/_11_Arithmetic_Expressions/Main.java
@@ -0,0 +1,27 @@
+package part_01._02_Types._11_Arithmetic_Expressions;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Basic arithmetic operations
+		int additionResult = 10 + 3;
+		System.out.println("10 + 3 = " + additionResult);
+
+		int divisionResult = 10 / 3;
+		System.out.println("10 / 3 = " + divisionResult);
+
+		// Increment operations
+		int x = 1;
+		x++; // ++x would produce the same result
+		System.out.println("x after x++: " + x);
+
+		int y1 = x++;
+		System.out.println("x after x++: " + x);
+		System.out.println("y after x++: " + y1);
+
+		int y2 = ++x;
+		System.out.println("x after ++x: " + x);
+		System.out.println("y1 after ++x: " + y2);
+	}
+
+}
