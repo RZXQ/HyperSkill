6d07387a0c6caa2824374ae4b96664f23bc2928b
Reacher <31989133+RZXQ@users.noreply.github.com>
2024-09-26 01:45:12 +0800

    Add Main.java to demonstrate constant declaration
    This commit introduces a new class, Main, which includes an example of defining a constant in Java using the `final` keyword. The constant `PI` is set to 3.14F, illustrating the immutability of `final` variables.

---
+++ java/java-mosh/src/part_01/_02_Types/_10_Constants/Main.java
@@ -0,0 +1,10 @@
+package part_01._02_Types._10_Constants;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Define a constant
+		final float PI = 3.14F; // PI is a constant, value cannot be changed
+	}
+
+}
