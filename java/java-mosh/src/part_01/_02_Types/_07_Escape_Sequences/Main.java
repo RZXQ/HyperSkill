fe6d51f1303b05c48ed89dcfbd4d100701f69b52
Reacher <31989133+RZXQ@users.noreply.github.com>
2024-09-26 01:09:12 +0800

    add: _07_Escape_Sequences java file

---
+++ java/java-mosh/src/part_01/_02_Types/_07_Escape_Sequences/Main.java
@@ -0,0 +1,13 @@
+package part_01._02_Types._07_Escape_Sequences;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Demonstrate escape sequences
+		System.out.println("Hello \"Ruizhi\"");
+		System.out.println("c:\\Windows\\...");
+		System.out.println("Hello\n");
+		System.out.println("Hello\t");
+	}
+
+}
