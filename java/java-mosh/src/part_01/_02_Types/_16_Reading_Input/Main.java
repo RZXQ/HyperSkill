fed5ade5e7753f0c6c483dda986391e2c8ee9a59
Reacher <31989133+RZXQ@users.noreply.github.com>
2024-09-26 02:22:44 +0800

    Add Main.java to read input from terminal
    Introduced a new Main.java file to handle various input types using Scanner. This file demonstrates reading byte, single word, and full line inputs from the terminal.

---
+++ java/java-mosh/src/part_01/_02_Types/_16_Reading_Input/Main.java
@@ -0,0 +1,29 @@
+package part_01._02_Types._16_Reading_Input;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Read input from the terminal (System.in)
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Age: ");
+		// scanner.nextByte():
+		byte age = scanner.nextByte();
+		System.out.println("You are " + age);
+
+		System.out.print("Name: ");
+		// scanner.next(): reads only one token (word) until whitespace
+		String name = scanner.next();
+		System.out.println(name);
+
+		System.out.print("Full Name: ");
+		// scanner.nextLine(): consumes the entire current line, including the newline
+		// character, and moves the cursor to the beginning of the next line
+		scanner.nextLine();
+		String fullName = scanner.nextLine().trim(); // method chaining
+		System.out.println(fullName);
+	}
+
+}
