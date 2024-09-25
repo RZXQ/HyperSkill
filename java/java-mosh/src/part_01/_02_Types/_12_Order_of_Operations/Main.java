0b5881ba7e08fc301b023da4ef3a1919747b09dc
Reacher <31989133+RZXQ@users.noreply.github.com>
2024-09-26 02:00:20 +0800

    Add Example for Order of Operations in Java
    Created a new Main.java file to demonstrate the precedence of arithmetic operations in Java. This example shows how multiplication is performed before addition, illustrating the concept with a simple calculation and output.

---
+++ java/java-mosh/src/part_01/_02_Types/_12_Order_of_Operations/Main.java
@@ -0,0 +1,11 @@
+package part_01._02_Types._12_Order_of_Operations;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// () > */ > + -
+		int x = 10 + 3 * 2;
+		System.out.println(x);
+	}
+
+}
