6f43ad52d46e70e3753a1a2d570237d77937943f
Reacher <31989133+RZXQ@users.noreply.github.com>
2024-09-26 00:24:36 +0800

    add: _04_Reference_Types java file

---
+++ java/java-mosh/src/part_01/_02_Types/_04_Reference_Types/Main.java
@@ -0,0 +1,13 @@
+package part_01._02_Types._04_Reference_Types;
+
+import java.util.Date;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Date now = new Date(); // when dealing with reference type, we always need to
+								// allocate memory
+		System.out.println(now);
+	}
+
+}
