4be286c0532191bdddd4dda54a3a635cf0290c71
Reacher <31989133+RZXQ@users.noreply.github.com>
2024-09-27 02:16:50 +0800

    Add basic for-loop example in new class
    Introduces a new Java class under the control flow section demonstrating a simple for-loop. This class prints "Hello World" with an incrementing index five times.

---
+++ java/java-mosh/src/part_01/_03_Control_Flow/_09_For_Loops/_09_For_Loops.java
@@ -0,0 +1,11 @@
+package part_01._03_Control_Flow._09_For_Loops;
+
+public class _09_For_Loops {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Hello World " + i);
+		}
+	}
+
+}
