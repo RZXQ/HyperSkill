03357e98f673ceef3f57d7b32935f5a4bedf79f6
Reacher <31989133+RZXQ@users.noreply.github.com>
2024-09-27 01:20:31 +0800

    Add Comparison Operators example to Control Flow section
    Introduced a new Java file demonstrating comparison operators within the Control Flow section. This example covers equality, inequality, and relational operations using simple integer comparisons.

---
+++ java/java-mosh/src/part_01/_03_Control_Flow/_02_Comparison_Operators/_02_Comparison_Operators.java
@@ -0,0 +1,16 @@
+package part_01._03_Control_Flow._02_Comparison_Operators;
+
+public class _02_Comparison_Operators {
+
+	public static void main(String[] args) {
+		int x = 1;
+		int y = 1;
+		System.out.println(x == y);
+		System.out.println(x != y);
+		System.out.println(x > y);
+		System.out.println(x >= y);
+		System.out.println(x < y);
+		System.out.println(x <= y);
+	}
+
+}
