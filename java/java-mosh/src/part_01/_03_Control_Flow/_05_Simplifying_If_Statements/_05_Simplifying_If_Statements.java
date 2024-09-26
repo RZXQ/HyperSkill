fd0c1749082dada867f9b41ef3a368acf0e15a37
Reacher <31989133+RZXQ@users.noreply.github.com>
2024-09-27 01:51:27 +0800

    Add example for simplifying if statements in Java
    This new example demonstrates how to simplify if statements by directly assigning boolean expressions to variables. It helps clarify control flow and improve code readability.

---
+++ java/java-mosh/src/part_01/_03_Control_Flow/_05_Simplifying_If_Statements/_05_Simplifying_If_Statements.java
@@ -0,0 +1,11 @@
+package part_01._03_Control_Flow._05_Simplifying_If_Statements;
+
+public class _05_Simplifying_If_Statements {
+
+	public static void main(String[] args) {
+		int income = 120_000;
+		boolean hasHighIncome = income > 100_000;
+		System.out.println(hasHighIncome);
+	}
+
+}
