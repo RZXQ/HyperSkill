90e38c6fe4e7a93fa57687609eeea06aae15f2b1
Reacher <31989133+RZXQ@users.noreply.github.com>
2024-09-30 14:48:17 +0800

    Add basic structure for Types of Errors class
    Created the scaffolding for discussing different error types, including comments outlining compile-time and run-time errors. The `main` method is currently empty, serving as a placeholder for future implementations.

---
+++ java/java-mosh/src/part_01/_05_Debugging_and_Deploying_Applications/_02_Types_of_Errors/_02_Types_of_Errors.java
@@ -0,0 +1,12 @@
+package part_01._05_Debugging_and_Deploying_Applications._02_Types_of_Errors;
+
+// two types of errors:
+// 1. compile-time errors
+// 2. run-time errors: bugs(using debugger)
+public class _02_Types_of_Errors {
+
+	public static void main(String[] args) {
+
+	}
+
+}
