6f353be9bc1900b456010232d55dcb3463c7ea42
Reacher <31989133+RZXQ@users.noreply.github.com>
2024-10-04 01:11:49 +0800

    Add new class for creating classes in Java
    Introduce a new class `_3_Creating_Classes` under the package `part_02._02_Classes`. This class contains a field `text` along with methods `setText` and `clear` to manipulate it. These methods demonstrate basic class functionality and object reference usage.

---
+++ java/java-mosh/src/part_02/_02_Classes/_3_Creating_Classes/_3_Creating_Classes.java
@@ -0,0 +1,15 @@
+package part_02._02_Classes._3_Creating_Classes;
+
+public class _3_Creating_Classes {
+
+	public String text; // Field
+
+	public void setText(String text) {
+		this.text = text; // this is a reference to the current object
+	}
+
+	public void clear() {
+		this.text = "";
+	}
+
+}
