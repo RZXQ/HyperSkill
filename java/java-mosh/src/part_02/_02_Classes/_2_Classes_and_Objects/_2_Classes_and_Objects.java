9c58ddc32516c9b4819057206d108b142f9348df
Reacher <31989133+RZXQ@users.noreply.github.com>
2024-10-04 01:05:31 +0800

    Create initial class for outlining OOP concepts
    Introduced a new class to delineate the basic principles of Object-Oriented Programming. This serves as a foundational element of the second module focused on Classes and Objects.

---
+++ java/java-mosh/src/part_02/_02_Classes/_2_Classes_and_Objects/_2_Classes_and_Objects.java
@@ -0,0 +1,11 @@
+package part_02._02_Classes._2_Classes_and_Objects;
+
+/// 1. Class: a blueprint for creating objects: Fields + Methods
+/// 2. Object: an instance of a class
+public class _2_Classes_and_Objects {
+
+	public static void main(String[] args) {
+
+	}
+
+}
