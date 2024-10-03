b1269e44ce06f1d98f2bdafe9f6323d5d91b8468
Reacher <31989133+RZXQ@users.noreply.github.com>
2024-10-04 00:26:00 +0800

    Add Course Structure class for OOP principles outline
    Provided a new class to outline the four principles of object-oriented programming: Encapsulation, Abstraction, Inheritance, and Polymorphism. This forms part of the foundational course structure in the Getting Started section.

---
+++ java/java-mosh/src/part_02/_01_Getting_Started/_4_Course_Structure/_4_Course_Structure.java
@@ -0,0 +1,12 @@
+package part_02._01_Getting_Started._4_Course_Structure;
+
+/*
+* Principals of Object-oriented programming:
+* 1. Encapsulation
+* 2. Abstraction
+* 3. Inheritance
+* 4. Polymorphism
+* */
+public class _4_Course_Structure {
+
+}
