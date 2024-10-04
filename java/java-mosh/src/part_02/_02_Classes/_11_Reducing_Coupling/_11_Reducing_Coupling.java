3b1a9beca268185bfc2075ef437f7d8ed063a5c7
Reacher <31989133+RZXQ@users.noreply.github.com>
2024-10-05 00:55:12 +0800

    Add Browser class with basic navigation functionality
    Implemented a Browser class that includes methods to find an IP address and send an HTTP request. Also added a driver class to demonstrate navigating to a URL. These changes aim to illustrate reducing coupling in the code.

---
+++ java/java-mosh/src/part_02/_02_Classes/_11_Reducing_Coupling/_11_Reducing_Coupling.java
@@ -0,0 +1,10 @@
+package part_02._02_Classes._11_Reducing_Coupling;
+
+public class _11_Reducing_Coupling {
+
+	public static void main(String[] args) {
+		Browser browser = new Browser();
+		browser.navigate("...");
+	}
+
+}
