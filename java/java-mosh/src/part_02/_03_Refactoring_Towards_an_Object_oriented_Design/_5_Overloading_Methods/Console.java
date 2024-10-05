b001ab617885ba8dbbafafa036ed458c87237e0a
Reacher <31989133+RZXQ@users.noreply.github.com>
2024-10-05 23:10:51 +0800

    Add detailed explanations for static and instance methods
    Enhanced documentation to clarify the differences between static and instance fields and methods. This includes access rules, memory allocation, and their behavior within the class.

--- java/java-mosh/src/part_02/_03_Refactoring_Towards_an_Object_oriented_Design/_5_Overloading_Methods/Console.java
+++ java/java-mosh/src/part_02/_03_Refactoring_Towards_an_Object_oriented_Design/_5_Overloading_Methods/Console.java
@@ -8,6 +8,22 @@ public class Console {
 	/// of Scanner shared by all methods in the Console class. This avoids the overhead of
 	/// creating a new Scanner object each time a method is called.
 	///
+	/// a. Static Fields and Methods:
+	/// Single Copy: Static fields and methods have a single copy that is shared across
+	/// all instances of the class (or even if there are no instances at all).
+	/// Class Level: They belong to the class itself rather than any specific instance.
+	/// When the class is loaded into memory, the static fields and static methods are
+	/// initialized.
+	/// Access: Static methods cannot directly access instance (non-static) fields or
+	/// methods. They can only directly access other static fields or methods.
+	///
+	/// b. Non-Static (Instance) Fields and Methods:
+	/// Separate Copies: Each instance of the class has its own copy of the instance
+	/// fields.
+	/// Instance Level: These fields and methods belong to the specific instance. They are
+	/// allocated in memory when the instance is created.
+	/// Access: Instance methods can access both instance fields and methods, as well as
+	/// static fields and methods.
 	///
 	/// 2. Thread issue:
 	/// there will be one static Scanner object in memory that is shared and used by both
