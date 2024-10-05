ca2c70544215237fc2a86063ff61e47cdc356af4
Reacher <31989133+RZXQ@users.noreply.github.com>
2024-10-05 23:19:23 +0800

    Create MortgageReport and MortgageCalculator classes
    Structured the mortgage calculation by introducing MortgageReport for output formatting and MortgageCalculator for computations. Added a Console class to handle user input, improving modularity and readability.

---
+++ java/java-mosh/src/part_02/_03_Refactoring_Towards_an_Object_oriented_Design/_6_Extracting_the_MortgageReport_Class/MortgageCalculator.java
@@ -0,0 +1,34 @@
+package part_02._03_Refactoring_Towards_an_Object_oriented_Design._6_Extracting_the_MortgageReport_Class;
+
+public class MortgageCalculator {
+
+	static final byte MONTHS_IN_YEAR = 12;
+	static final byte PERCENT = 100;
+
+	public static void main(String[] args) {
+		int principal = (int) Console.readNumber("Principal: ", 1000, 1_000_000);
+		float annualInterest = (float) Console.readNumber("Annual Interest Rate: ", 1, 30);
+		byte years = (byte) Console.readNumber("Period (Years): ", 1, 30);
+
+		MortgageReport.printMortgage(principal, annualInterest, years);
+		MortgageReport.printPaymentSchedule(years, principal, annualInterest);
+	}
+
+	public static double calculateMortgage(int principal, float annualInterest, byte years) {
+		float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+		short numberOfPayments = (short) (years * MONTHS_IN_YEAR);
+		return principal * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+				/ (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+	}
+
+	public static double calculateBalance(int principal, float annualInterest, byte years, short numberOfPaymentsMade) {
+		float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+		short numberOfPayments = (short) (years * MONTHS_IN_YEAR);
+		double balance = principal
+				* (Math.pow((1 + monthlyInterest), numberOfPayments)
+						- (Math.pow((1 + monthlyInterest), numberOfPaymentsMade)))
+				/ (Math.pow((1 + monthlyInterest), numberOfPayments) - 1);
+		return balance;
+	}
+
+}
---
+++ java/java-mosh/src/part_02/_03_Refactoring_Towards_an_Object_oriented_Design/_6_Extracting_the_MortgageReport_Class/MortgageReport.java
@@ -0,0 +1,26 @@
+package part_02._03_Refactoring_Towards_an_Object_oriented_Design._6_Extracting_the_MortgageReport_Class;
+
+import java.text.NumberFormat;
+
+public class MortgageReport {
+
+	public static void printMortgage(int principal, float annualInterest, byte years) {
+		double mortgage = MortgageCalculator.calculateMortgage(principal, annualInterest, years);
+		String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+		System.out.println();
+		System.out.println("MORTGAGE");
+		System.out.println("--------");
+		System.out.println("Monthly Payments: " + mortgageFormatted);
+	}
+
+	public static void printPaymentSchedule(byte years, int principal, float annualInterest) {
+		System.out.println();
+		System.out.println("PAYMENT SCHEDULE");
+		System.out.println("----------------");
+		for (short month = 0; month < years * MortgageCalculator.MONTHS_IN_YEAR; month++) {
+			double balance = MortgageCalculator.calculateBalance(principal, annualInterest, years, month);
+			System.out.println(NumberFormat.getCurrencyInstance().format(balance));
+		}
+	}
+
+}
