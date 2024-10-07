dc01a77b368b0e88e52cab17e8935c83595d0409
Reacher <31989133+RZXQ@users.noreply.github.com>
2024-10-08 02:26:24 +0800

    Refactor method overloading documentation
    Clarified the definition and key points of method overloading. Emphasized the significance of parameter lists and highlighted that return type is not a factor in overloading.

--- java/java-mosh/src/part_02/_02_Classes/_13_Method_Overloading/_13_Method_Overloading.java
+++ java/java-mosh/src/part_02/_02_Classes/_13_Method_Overloading/_13_Method_Overloading.java
@@ -1,6 +1,20 @@
 package part_02._02_Classes._13_Method_Overloading;
 
-/// Method overloading: Defining multiple methods with the same name but different parameter lists (different number, type, or order of parameters) within the same class.
+///  **Method overloading** is determined by the method name and the parameter list (the number, type, and order of parameters).
+///
+/// #### Key Points:
+///
+/// 1. **Method Overloading**:
+///    - Method overloading allows a class to have multiple methods with the same name but different parameter lists.
+///    - The parameter lists must differ in at least one of the following ways:
+///      - Number of parameters.
+///      - Type of parameters.
+///      - Order of parameters.
+///
+/// 2. **Return Type**:
+///    - The return type is not considered when determining method overloading.
+///    - Two methods cannot be overloaded solely by differing return types.
+///    - If two methods have the same name and parameter list but different return types, it will result in a compile-time error.
 public class _13_Method_Overloading {
 
 	public static void main(String[] args) {
