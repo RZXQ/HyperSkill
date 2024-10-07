98f9fe285955309ff2cb1b153d130b2068c076f1
Reacher <31989133+RZXQ@users.noreply.github.com>
2024-10-08 01:58:35 +0800

    Rename Checkbox to CheckBox
    Updated the class name and filename to maintain naming consistency. This change ensures that all references to the class use the proper camel case format.

--- java/java-mosh/src/part_02/_04_Inheritance/_10_Abstract_Classes_and_Methods/CheckBox.java
+++ java/java-mosh/src/part_02/_04_Inheritance/_10_Abstract_Classes_and_Methods/CheckBox.java
@@ -1,6 +1,6 @@
 package part_02._04_Inheritance._10_Abstract_Classes_and_Methods;
 
-public class Checkbox extends UIControl {
+public class CheckBox extends UIControl {
 
 	@Override
 	public void render() {
