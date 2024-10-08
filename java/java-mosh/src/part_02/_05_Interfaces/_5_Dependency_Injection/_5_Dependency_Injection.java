1a0842a08008b0f78b530955292cef0832779ec4
Reacher <31989133+RZXQ@users.noreply.github.com>
2024-10-09 02:14:12 +0800

    Add Dependency Injection class with examples
    Introduced a new class to demonstrate dependency injection concepts, including constructor, setter, and method injection. This sets up the framework for explaining best practices in managing dependencies in Java applications.

---
+++ java/java-mosh/src/part_02/_05_Interfaces/_5_Dependency_Injection/_5_Dependency_Injection.java
@@ -0,0 +1,15 @@
+package part_02._05_Interfaces._5_Dependency_Injection;
+
+/// Dependency injection: our classes should not instantiate their dependencies
+///
+/// Three ways to inject dependency:
+/// 1. Constructor injection
+/// 2. Setting injection
+/// 3. Method injection
+public class _5_Dependency_Injection {
+
+	public static void main(String[] args) {
+
+	}
+
+}
