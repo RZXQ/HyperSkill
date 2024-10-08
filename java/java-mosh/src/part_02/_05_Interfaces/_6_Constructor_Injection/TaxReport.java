8e9bcf9cf9fce230690a4b2e63a1f7e9e7b8d578
Reacher <31989133+RZXQ@users.noreply.github.com>
2024-10-09 02:53:37 +0800

    Update comment to clarify constructor injection method
    Modified the comment to specify that constructor injection is the more common approach, enhancing understanding for readers about why constructor injection is preferred in this context. This improves code readability and maintainability by providing clearer guidance.

--- java/java-mosh/src/part_02/_05_Interfaces/_6_Constructor_Injection/TaxReport.java
+++ java/java-mosh/src/part_02/_05_Interfaces/_6_Constructor_Injection/TaxReport.java
@@ -3,7 +3,7 @@ package part_02._05_Interfaces._6_Constructor_Injection;
 /// ## Programming against interfaces:
 /// TaxReport does not know about the specific implementation of TaxCalculator. It operates using the TaxCalculator interface, which makes the code more flexible and maintainable. This approach is known as programming against interfaces.
 ///
-/// ## Constructor injection:
+/// ## Constructor injection(more common approach):
 /// Constructor injection is used here to inject the dependency. We avoid creating a new TaxCalculator object using the new keyword.
 public class TaxReport {
 
