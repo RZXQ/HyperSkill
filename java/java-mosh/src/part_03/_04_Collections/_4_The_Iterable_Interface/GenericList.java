dfd95d4a4fe3a26af58aed5e2427c99ccbf1876d
Reacher <31989133+RZXQ@users.noreply.github.com>
2024-10-18 01:07:58 +0800

    Refactor variable names in GenericList class
    Renamed 'list' to 'items' for better clarity and consistency. This change impacts the internal array that stores the list items and all methods interacting with it.

--- java/java-mosh/src/part_03/_04_Collections/_4_The_Iterable_Interface/GenericList.java
+++ java/java-mosh/src/part_03/_04_Collections/_4_The_Iterable_Interface/GenericList.java
@@ -4,16 +4,16 @@ import java.util.Iterator;
 
 public class GenericList<T> implements Iterable<T> {
 
-	private T[] list = (T[]) new Object[10];
+	private T[] items = (T[]) new Object[10];
 
 	private int count;
 
 	public void add(T item) {
-		list[count++] = item;
+		items[count++] = item;
 	}
 
 	public T get(int index) {
-		return list[index];
+		return items[index];
 	}
 
 	@Override
