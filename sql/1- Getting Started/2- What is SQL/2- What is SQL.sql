45d12109a0af2b898f40caef0cc090d073fb3654
Reacher <31989133+RZXQ@users.noreply.github.com>
2024-10-18 02:00:49 +0800

    Add introductory SQL explanation script
    Creates a new SQL script file explaining fundamental database concepts. The script introduces databases, distinguishes between DBMS types, and clarifies relational and NoSQL systems.

---
+++ sql/1- Getting Started/2- What is SQL/2- What is SQL.sql
@@ -0,0 +1,5 @@
+-- A Database: is a collection of data stored in a format that can easily be accessed
+
+-- DBMS: Database management system
+-- 1. Relational (RDBMS): Stored in tables, using SQL (structured query language), e.g., MySQL, Oracle
+-- 2. NoSQL: NoSQL systems don't use SQL; they do not have tables or relationships, e.g., MongoDB, Cassandra, Redis
