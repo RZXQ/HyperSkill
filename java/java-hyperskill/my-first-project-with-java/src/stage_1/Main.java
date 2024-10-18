f768b63beae91f17a3753b47bfc783b6ac97ab17
Reacher <31989133+RZXQ@users.noreply.github.com>
2024-10-19 01:07:05 +0800

    Add initial Main.java with product prices listing
    This commit introduces the Main.java file in the stage_1 package. The file includes a main method that prints the prices of various products.

---
+++ java/java-hyperskill/my-first-project-with-java/src/stage_1/Main.java
@@ -0,0 +1,15 @@
+package stage_1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Prices:");
+		System.out.println("Bubblegum: $2");
+		System.out.println("Toffee: $0.2");
+		System.out.println("Ice cream: $5");
+		System.out.println("Milk chocolate: $4");
+		System.out.println("Doughnut: $2.5");
+		System.out.println("Pancake: $3.2");
+	}
+
+}
