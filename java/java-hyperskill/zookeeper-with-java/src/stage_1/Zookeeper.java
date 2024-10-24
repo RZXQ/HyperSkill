9c7879871d49d5940463442c14b28097f89e9df5
Reacher <macryze@icloud.com>
2024-10-24 18:08:33 +0800

    Add initial Zookeeper class with main method
    This commit introduces the Zookeeper class in the 'stage_1' package. The main method in this class prints messages about the status of various animals to the console.

---
+++ java/java-hyperskill/zookeeper-with-java/src/stage_1/Zookeeper.java
@@ -0,0 +1,13 @@
+package stage_1;
+
+public class Zookeeper {
+
+	public static void main(String[] args) {
+		System.out.println("I love animals!");
+		System.out.println("Let's check on the animals...");
+		System.out.println("The deer looks fine.");
+		System.out.println("The bat looks happy.");
+		System.out.println("The lion looks healthy.");
+	}
+
+}
