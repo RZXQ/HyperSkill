d2a178724e7ac7e1c18ac11afb6dfdc5114248ea
Reacher <macryze@icloud.com>
2024-10-24 18:55:06 +0800

    Add Zookeeper class to display camel ASCII art
    This commit introduces the Zookeeper class in stage_2, containing a main method that prints an ASCII art representation of a camel. This feature enhances the visual output of the program by providing a static image of a camel when executed.

---
+++ java/java-hyperskill/zookeeper-with-java/src/stage_2/Zookeeper.java
@@ -0,0 +1,30 @@
+package stage_2;
+
+public class Zookeeper {
+
+	public static void main(String[] args) {
+		String camel = """
+				Switching on the camera in the camel habitat...
+				 ___.-''''-.
+				/___  @    |
+				',,,,.     |         _.'''''''._
+				     '     |        /           \\
+				     |     \\    _.-'             \\
+				     |      '.-'                  '-.
+				     |                               ',
+				     |                                '',
+				      ',,-,                           ':;
+				           ',,| ;,,                 ,' ;;
+				              ! ; !'',,,',',,,,'!  ;   ;:
+				             : ;  ! !       ! ! ;  ;   :;
+				             ; ;   ! !      ! !  ; ;   ;,
+				            ; ;    ! !     ! !   ; ;
+				            ; ;    ! !    ! !     ; ;
+				           ;,,      !,!   !,!     ;,;
+				           /_I      L_I   L_I     /_I
+				Look at that! Our little camel is sunbathing!""";
+
+		System.out.println(camel);
+	}
+
+}
