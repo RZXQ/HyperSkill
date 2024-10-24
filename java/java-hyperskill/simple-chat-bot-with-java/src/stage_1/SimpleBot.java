b93825b54782c81192932f640e77e144506bf257
Reacher <31989133+RZXQ@users.noreply.github.com>
2024-10-24 16:36:34 +0800

    Add initial SimpleBot implementation
    Created a basic SimpleBot class in Java with a main method that prints a greeting and creation year. This acts as a foundation for further development of the chatbot functionality.

---
+++ java/java-hyperskill/simple-chat-bot-with-java/src/stage_1/SimpleBot.java
@@ -0,0 +1,10 @@
+package stage_1;
+
+public class SimpleBot {
+
+	public static void main(String[] args) {
+		System.out.printf("Hello! My name is %s\n", "Aid");
+		System.out.printf("I was created in %d\n", 2024);
+	}
+
+}
